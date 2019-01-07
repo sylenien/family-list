ee40afd1d837d0456542904a76a62a3b158b2f5b
Nikita Melnikov <sylenien@gmail.com>
2019-01-07 18:55:21 +0300

    store connection

--- src/components/List.js
+++ src/components/List.js
@@ -2,21 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const List = styled.div`
+import ListEntry from './ListEntry';
+
+const ListEl = styled.div`
   display: flex;
   height: 100%;
 `;
 
 
-const List = ({ entryes }) => (
-  <List>
-    {entryes.map(item => (
-        <Entry {...item} />
+const List = ({list}) => (
+  <ListEl>
+    {list.entryes.map(item => (
+        <ListEntry {...item} />
       )
     )}
-  </List>
+  </ListEl>
 ) 
 
 List.propTypes = ({
   entryes: PropTypes.arrayOf(PropTypes.object)
 })
+
+export default List;
--- src/components/ListEntry.js
+++ src/components/ListEntry.js
@@ -5,3 +5,11 @@ import PropTypes from 'prop-types';
 const Entry = styled.div`
   align-self: center;
 `;
+
+const ListEntry = (entry) => (
+  <Entry>
+    {entry}
+  </Entry>
+)
+
+export default ListEntry;
