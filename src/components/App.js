5e1c7fe0c4bd3c09187a6bd07bcb22dcb57f4aa6
Nikita Melnikov <sylenien@gmail.com>
2019-01-07 21:39:50 +0300

    adding screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 // import styled from 'styled-components';
 
-import List from '../containers/List';
+import Home from './Home';
+import Add from '../containers/Add';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <List />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/add" component={Add} />
+        </div>
+      </Router>
     );
   }
 }
