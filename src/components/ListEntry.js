86ba464d68cdd6e13f8b338c74697da5f8b075cb
Наталья <desu@MBP-Natala.sknt.ru>
2019-01-08 17:10:01 +0300

    merged

--- src/components/ListEntry.js
+++ src/components/ListEntry.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 import smoji from '../emoji.js';
-=======
-import emoji from 'react-easy-emoji';
->>>>>>> 17db41721ac206d8fd033773eaa9c2b45e73383a
 
 const Entry = styled.div`
   align-self: center;
@@ -23,33 +19,17 @@ const EntryChildRight = styled(EntryChild)`
   text-align: right;
 `;
 
-<<<<<<< HEAD
 const FormIcon = styled.div`
   display: inline;
   cursor: pointer;
   margin-right: 0.3em;
 `;
-=======
-
-function smoji (input) {
-	return emoji(input, {
-		baseUrl: '//twemoji.maxcdn.com/2/svg',
-    ext: '.svg',
-    protocol: 'https:',
-    size: "",
-	})
-}
->>>>>>> 17db41721ac206d8fd033773eaa9c2b45e73383a
 
 const ListEntry = (entry) => {
   const {price, name, icon } = entry; // date, id
   return (
   <Entry>
-<<<<<<< HEAD
     <FormIcon>{smoji(icon)}</FormIcon>
-=======
-    {smoji(icon)}
->>>>>>> 17db41721ac206d8fd033773eaa9c2b45e73383a
     <EntryChild>{name}</EntryChild>
     <EntryChildRight>{price}</EntryChildRight>
     {/* <EntryChild>{date}</EntryChild> */}
