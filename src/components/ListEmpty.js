8701487008888c24193298b1c23eadd79ead80cd
Наталья <desu@MBP-Natala.sknt.ru>
2019-01-08 16:20:34 +0300

    Empty list

---
+++ src/components/ListEmpty.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+// const Empty = styled.div`
+//   align-self: center;
+// `;
+
+const ListEmpty = () => (
+  <div>
+    <h1>Oops! There is nothing here yet.</h1>
+    <h3>Add new records via button below.</h3>
+  </div>
+)
+
+export default ListEmpty;
